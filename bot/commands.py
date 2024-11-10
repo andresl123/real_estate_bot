6164271f94ce608bb162701983a7c0b00b0efa75
andresl123 <andreslopes2009@gmail.com>
2024-11-10 09:56:02 -0500

    adding stop command

--- bot/commands.py
+++ bot/commands.py
@@ -10,18 +10,30 @@ import time
 # Dictionary to keep track of user state
 user_data = {}
 
-# Step 1: Handle '/start' command
+# Handle '/start' command
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     chat_id = message.chat.id
     welcome_message = (
         "Welcome to the Bot! 🤖\n"
         "I can help you search for information based on your location and budget.\n"
-        "Use the /search command to get started. or you ask any question to me"
+        "Use the /search command to get started. To stop the automatic search use /stop"
     )
     bot.send_message(chat_id, welcome_message)
 
-# Step 1: Handle '/search' command
+# Handle '/stop' command
+# Decorator will handle the user input command, in this case "stop". This command will stop the while loop (in line 118). 
+@bot.message_handler(commands=['stop'])
+# The above decorator will call the bellow function
+def handle_search(message):
+    chat_id = message.chat.id
+    user_info = user_data.get(chat_id)
+    bot.send_message(chat_id, "The search was stoped")
+
+    # The while loop will stop because the step value will change to stop.
+    user_info['step'] = 'stop'
+
+# Handle '/search' command
 # Decorator will handle the user input command, in this case "search"
 @bot.message_handler(commands=['search'])
 # The above decorator will call the bellow function
@@ -32,7 +44,7 @@ def handle_search(message):
     # Initialize user state to capture menu first
     user_data[chat_id] = {'step': 'menu'}
 
-# Step 2: Handle user responses
+# Handle user responses
 # the bot decorator is checking new messages that arrive in the telegram bot.
 # lambda function will check if in the "message" has a "chat.id" in dictionary "user_data" 
 @bot.message_handler(func=lambda message: message.chat.id in user_data)
@@ -102,17 +114,23 @@ def handle_user_input(message):
             # Check if the user input is within the valid range
             if user_data[chat_id]['city'].replace(" ","").isalpha():
                 print(user_data)
-
+                
+                # This while loop will stop when the key step has the value stop
                 while (True):
-                    main = Main(user_data[chat_id]['menu'],user_data[chat_id]['location'],user_data[chat_id]['budget'],user_data[chat_id]['city'])
-                    main.findList()
+                    if ( user_info['step'] == 'stop' ):
+                        break
+                    
+                    else:
+                        main = Main(user_data[chat_id]['menu'],user_data[chat_id]['location'],user_data[chat_id]['budget'],user_data[chat_id]['city'])
+                        main.findList()
 
-                    # # Reset the use`r state
-                    # user_data.pop(chat_id)
-                    select = Select
-                    for item in select.select():
-                        bot.send_message(chat_id, item)
-                    time.sleep(10)
+                        select = Select
+                        for item in select.select():
+                            bot.send_message(chat_id, item)
+                        time.sleep(10)
+                # when the while finishes will ask again to the user choose between 1 and 3 and change key step to menu, that will receive the user input to do another search
+                bot.send_message(chat_id, "Select one of the option for a NEW SEARCH:\nCategory menu:\n1. House\n2. Condo\n3. Land\nChoose between 1 and 3")
+                user_info['step'] = 'menu'
 
             else:
                 bot.send_message(chat_id, "Invalid input!! Enter your desired city:")
